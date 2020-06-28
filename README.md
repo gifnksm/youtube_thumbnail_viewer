81c5b7cccfcf6529987db56dba0006bc5a0b9ce8
gifnksm <makoto.nksm+github@gmail.com>
2020-06-28 16:53:15 +0900

    First implementation

---
+++ README.md
@@ -0,0 +1,7 @@
+# YouTube Thumbnail Viewer
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Adds a YouTube thumbnail preview icon to the address bar.
+
+![Screenshot](./screenshot.png)
---
+++ popup/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const thumbnail = document.getElementById("thumbnail");
+const error = document.getElementById("error");
+
+const queryActiveTab = async () => {
+    let tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    if (tabs.length === 0) {
+        throw new Error("No active tabs found");
+    }
+    return tabs[0];
+};
+
+const getVideoId = tab => {
+    let id = new URL(tab.url).searchParams.get("v");
+    if (id === null) {
+        throw new Error("No video ID found");
+    }
+    return id;
+};
+
+const loadImage = async (src) => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = (e) => reject(e);
+    img.src = src;
+});
+
+const main = async () => {
+    let tab = await queryActiveTab();
+    let id = getVideoId(tab);
+    let filenames = [["maxresdefault", 1280], ["sddefault", 640], ["hqdefault", 480], ["default", 120]];
+    for (let [filename, expected_width] of filenames) {
+        let src = `https://img.youtube.com/vi/${id}/${filename}.jpg`;
+        thumbnail.alt = src;
+        let img = await loadImage(src);
+        if (img.width != expected_width) {
+            continue;
+        }
+        thumbnail.src = img.src;
+        break;
+    }
+};
+
+main().catch(e => {
+    error.textContent = `Unexpected error: ${e.message}\n(${e.fileName}:${e.lineNumber}:${e.columnNumber})`;
+});
