5a81e2d574d83b957c5d20c4a9edd33320053e2f
evrial <evrial@gmail.com>
2026-03-15 11:05:54 +0000

    Define the hierarchy of thumbnails to try

--- popup/index.js
+++ popup/index.js
@@ -1,19 +1,40 @@
+"use strict";
+
 const thumbnail = document.getElementById("thumbnail");
 
-const updateThumbnailByUrl = async () => {
+const getVariants = (videoId, isShorts) => {
+  const base = `https://img.youtube.com/vi/${videoId}/`;
+  return isShorts 
+    ? [`${base}oardefault.jpg`, `${base}hq720.jpg`]
+    : [`${base}maxresdefault.jpg`, `${base}hqdefault.jpg`];
+};
+
+const updateThumbnailUI = async () => {
   const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
   const url = new URL(tab.url);
   
   // Extract ID
-  let videoId = url.searchParams.get("v") || url.pathname.split('/shorts/')[1];
+  const videoId = url.searchParams.get("v") || url.pathname.split('/shorts/')[1];
   if (!videoId) return;
 
   const isShorts = tab.url.includes('/shorts/');
   
-  // Use the reliable thumbnail endpoints
-  thumbnail.src = isShorts 
-    ? `https://i.ytimg.com/vi/${videoId}/oardefault.jpg`
-    : `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+  // Define the hierarchy of thumbnails to try
+  const variants = getVariants(videoId, isShorts);
+
+  let index = 0;
+
+  // Set the error handler to move to the next index in the array
+  thumbnail.onerror = () => {
+    index++;
+    if (index < variants.length) {
+      console.log(`Thumbnail ${index} failed, trying: ${variants[index]}`);
+      thumbnail.src = variants[index];
+    }
+  };
+  
+  // Start the chain
+  thumbnail.src = variants[0];
 };
 
-updateThumbnailByUrl();
+updateThumbnailUI();
